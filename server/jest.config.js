86b305eb19180b5b9a1fc3ea83712013faf0e44b
CFisher46 <69768522+CFisher46@users.noreply.github.com>
2025-06-13 19:28:36 +0100

    Jest Config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,11 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node"
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg
+  }
 };
