3e6cd070f09a352b65665d0d1b072a108014a5c1
CFisher46 <69768522+CFisher46@users.noreply.github.com>
2025-09-12 22:47:04 +0100

    prettier config

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,31 +1,20 @@
-import eslint from 'eslint';
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
-
-const { defineConfig } = eslint;
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: {
-      js,
-      prettier: prettierPlugin,
-    },
-    extends: ['js/recommended', 'plugin:prettier/recommended'],
+    plugins: { js },
+    extends: ['js/recommended'],
     rules: {
-      'constructor-super': 'off',
-      'prettier/prettier': 'error',
+      'constructor-super': 'off', // Disable the problematic rule
     },
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    languageOptions: {
-      globals: globals.browser,
-    },
+    languageOptions: { globals: globals.browser },
   },
-  ...tseslint.configs.recommended,
-  prettier,
+  tseslint.configs.recommended,
 ]);
